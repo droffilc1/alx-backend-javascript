0e28f5d752759f6358f63be4700c2c0bafd2a159
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-04-23 18:50:50 +0300

    fix: catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-      console.log('Got a response from the API');
-    } else {
-      reject(new Error({}));
-      console.log('Got a response from the API');
-    }
-  });
+  return promise
+    .then((result) => ({ status: 'success', data: result }))
+    .catch((error) => ({ status: 'error', error: error.message }))
+    .finally(() => console.log('Got a response from the API'));
 }
