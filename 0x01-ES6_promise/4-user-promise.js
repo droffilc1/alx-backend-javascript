5420e1593ac14073457dfcaf6de3625693726a2c
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-04-23 18:35:19 +0300

    feat: handle multiple promises

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    if (firstName && lastName) {
-      resolve({ firstName, lastName });
-    } else {
-      reject(new Error('failed'));
-    }
-  });
+  return Promise.resolve({ firstName, lastName });
 }
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(), uploadPhoto()])
+    .then((results) => results.forEach((result) => result.status));
+}
