b9e7ede1d8187f81543220ead94710dc4290fef3
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-04-24 10:21:53 +0300

    fix: handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,9 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const [photo, user] = values;
-      const logMessage = `${photo.body} ${user.firstName} ${user.lastName}`;
-      console.log(logMessage);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch((error) => {
-      console.log('Signup system offline', error);
+    .catch(() => {
+      console.error('Signup system offline');
     });
 }
