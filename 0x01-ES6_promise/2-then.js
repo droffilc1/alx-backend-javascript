85b0a5161cc7ba5b718fee386bea67c73b722ed4
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-04-24 10:03:48 +0300

    fix: requirement fail

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,6 +2,6 @@ export default function handleResponseFromAPI(promise) {
   const body = { status: 200, body: 'success' };
   return promise
     .then(() => body)
-    .catch((error) => new Error())
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
