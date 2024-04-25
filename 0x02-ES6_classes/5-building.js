9a293415724bb9784becaee9ec81678f9a468691
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-04-25 15:04:24 +0300

    fix: a building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
     this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
@@ -18,7 +16,7 @@ export default class Building {
     }
   }
 
-  static evacuationWarningMessage() {
-    throw new Error('Method evacuationWarningMessage() must tbe implemented');
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
