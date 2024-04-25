d237aba098acc1bb795cb1e042a59bd5388f8fb5
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-04-25 08:11:03 +0300

    fix: Handle multiple promises mandatory

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return [{ status: result.status, value: result.value }];
-      }
-      return [{ status: result.status, reason: result.reason }];
-    }));
+    .then((results) => {
+      const resultsArray = results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+      return resultsArray;
+    });
 }
