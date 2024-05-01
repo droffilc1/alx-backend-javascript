eb68926f162c409772068e519452cce259ee219f
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-01 15:41:20 +0300

    feat: creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -56,7 +56,31 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
 }
 
+/**
+ * @param employee
+ * @returns used as type predicate and if employee is a director
+ */
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+/**
+ * @param employee
+ * @returns call to specific methods
+ */
+function executeWork(employee: Teacher | Director): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
 console.log(createEmployee(200), Teacher);
 console.log(createEmployee(1000), Director);
 console.log(createEmployee('$500'), Director);
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+
 
