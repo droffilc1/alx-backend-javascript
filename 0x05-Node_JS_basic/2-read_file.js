6160429c26830f32342dd927558bf4d88dcd7074
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-18 10:43:17 +0300

    feat: reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+// Reads a file synchronously with Node JS
+const fs = require('node:fs');
+const path = require('node:path');
+
+module.exports = function countStudents(filepath) {
+  try {
+    const data = fs.readFileSync(path.resolve(filepath), 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const numberOfStudents = lines.length - 1;
+    console.log(`Number of students: ${numberOfStudents}`);
+
+    const students = lines.slice(1).map((line) => line.split(','));
+    const fields = {};
+
+    students.forEach((student) => {
+      const firstname = student[0];
+      const field = student[3];
+      if (field in fields) {
+        fields[field].push(firstname);
+      } else {
+        fields[field] = [firstname];
+      }
+    });
+
+    for (const [field, firstnames] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error(`Cannot load the database: ${err.message}`);
+  }
+};
