7c91e7c3a4747bbac9eb4f3fe29e17e2d6d2fddb
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-20 20:07:06 +0300

    fix: Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,36 +2,15 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('should return 4 when 1 and 3 are passed', function () {
+describe('calculateNumber', () => {
+  it('should return sum of two numbers rounded', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when 1 and 3.7 are passed', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when 1.2 and 3.7 are passed', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when 1.5 and 3.7 are passed', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should return 5 when 2.4 and 2.6 are passed', function () {
     assert.strictEqual(calculateNumber(2.4, 2.6), 5);
-  });
-
-  it('should return 6 when 2.5 and 3.4 are passed', function () {
     assert.strictEqual(calculateNumber(2.5, 3.4), 6);
-  });
-
-  it('should return -1 when -1.4 and 0.4 are passed', function () {
     assert.strictEqual(calculateNumber(-1.4, 0.4), -1);
-  });
-
-  it('should return 0 when 0 and 0 are passed', function () {
     assert.strictEqual(calculateNumber(0, 0), 0);
   });
 });
