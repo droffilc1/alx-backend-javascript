2518a32a268b3c83e7002ff1cc602f1bfc02ef35
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-21 10:29:00 +0300

    feat: stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+// Stubs
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+
+  it('should call calculateNumber with correct arguments', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    // verify that calculateNumber was called once with the correct arguments
+    sinon.assert.calledOnce(calculateNumberStub);
+    sinon.assert.calledWith(calculateNumberStub, 'SUM', 100, 20);
+
+    // Verify that console.log was called with the correct message
+    sinon.assert.calledOnce(consoleLogSpy);
+    sinon.assert.calledWith(consoleLogSpy, 'The total is: 10');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+});
